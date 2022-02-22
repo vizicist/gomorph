a86d6582c2b8b071d505819155f68d274bd3104c
Tim Thompson <vizicist@gmail.com>
2022-02-22 13:55:49 -0800

    more debugging

---
+++ newtest/morphtest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sensel.h>
+
+SenselDeviceList sensellist;
+
+int main()
+{
+	senselGetDeviceList(&sensellist);
+	int ndevices = sensellist.num_devices;
+	printf("ndevices = %d\n",ndevices);
+	int n;
+	for ( n=0; n<ndevices; n++ ) {
+		printf("n=%d serial_num=%s\n",n,sensellist.devices[n].serial_num);
+		unsigned char idx = sensellist.devices[n].idx;
+		SENSEL_HANDLE h;
+		printf("Trying to open device with idx = %d\n",idx);
+		SenselStatus status = senselOpenDeviceByID(&h,idx);
+		printf("status = %d  handle = %ld\n",status,(long long)h);
+	}
+	return(0);
+}
